27fee40ba3a1947b6c1e10c47820b0acf4fde97a
julio <juliosaki69@gmail.com>
2024-06-16 08:19:07 -0400

    Script_to_client

---
+++ script1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Autor: Julio
+#Date:June 2024
+#collecting client's information
+echo "Hello, please provide us some information!"
+read -p "your name:" name
+read -p "your age:" age
+read -p "your city:" city
+read -p "your profession:" profession
+# Displaying a message with the information gathered from the client
+echo " Hello your name is $name, your are $age years old living in $city and your profession is $profession."
+#end
